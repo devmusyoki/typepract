84200cbdc663a32851fc61db5b76675a1a049752
smusyoki <markmusyoki6@gmail.com>
2022-03-08 08:50:12 +0300

    Type Inference

--- app.ts
+++ app.ts
@@ -22,7 +22,7 @@
 // console.log(result);
 
 
-function add(n1 : number, n2 : number, showResult: boolean) {
+function add(n1 : number, n2 : number, showResult: boolean, phrase: string) {
  const result = n1 + n2;
 if (showResult){
     console.log(n1 + n2);
@@ -35,6 +35,6 @@ const number2 = 2.8;
 const printResult = true
 const resultPhrase = 'Result is: ';
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
 
 
