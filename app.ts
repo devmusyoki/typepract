d52fb01beeb71bb554b783ec3a70a134a2296824
smusyoki <markmusyoki6@gmail.com>
2022-03-21 07:47:01 +0300

    update

--- app.ts
+++ app.ts
@@ -1,8 +1,22 @@
-const person : {
+const person :{
     name: string;
     age: number;
+    hobbies: string[];
+    role: [number, string]
 } = {
-    name: 'Maximilian',
-    age: 30
+    name: Maximilian;
+    age: 30;
+    hobbies: ['Sports', 'Cooking']
+    role: [2, 'author']
 };
-console.log(person)
+
+person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies ) {
+    console.log(hobby.toUpperCase());
+}
