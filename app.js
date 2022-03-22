20ad67944a4cab3c5a3b5189aa3e61912bde3d13
smusyoki <markmusyoki6@gmail.com>
2022-03-22 08:13:59 +0300

    update

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var person = {
-    name: 'Maximilian',
-    age: 30
-};
-console.log(person);
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
