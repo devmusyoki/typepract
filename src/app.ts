0449126fddd907e662cfe4289d8bfee80b6c8655
Musyoki <markmusyoki6@gmail.com>
2022-03-28 09:07:23 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
-const userName = 'MarkMillan';
+class Department {
+   name : string; 
+   employees: string[];
 
-console.log(userName);
+   constructor(n: string) {
+       this.name = n;
+   }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee('Mark')
+accounting.addEmployee('Musyoki')
+
+describe(this: Department){
+    console.log('Department: ' + this.name);
+}
+
+printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+}
+addEmployee(employee:string){
+    this.employees.push(employee); 
+}
+accounting.describe();
+
+// const accountingCopy = {name: 'DUMMY', describe:accounting.describe}
+
+// accountingCopy.describe();
+
